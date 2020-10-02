bc6b25c4000113f48efd4bc7a41e928664464118
leoreisdias <leoanrdord99@gmail.com>
2020-10-02 15:19:15 -0300

    property binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  url: string = "www.google.com";
+
+  angular: boolean = true;
+
+  urlImage = 'https://jjm-upload.s3.amazonaws.com/3dcfa9e8e0d399972408099fb9189921-xbox-series-x-jogos-exclusivos_jar5.jpg'
+
+  getValor() {
+    return 1;
+  }
+
+  getAngular() {
+    return true;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
