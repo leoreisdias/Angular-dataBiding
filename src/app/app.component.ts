4f856034178f21e9a7085c20d727cece55321246
leoreisdias <leoanrdord99@gmail.com>
2020-10-04 13:26:31 -0300

    life-cycle of components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-biding';
+  valor: number = 5;
+
+  deletarCiclo: boolean = false;
+
+  mudarValor() {
+    this.valor++;
+  }
+
+  destroyCycle() {
+    this.deletarCiclo = true;
+  }
 }
--- src/app/ciclo-vida/ciclo-vida.component.ts
+++ src/app/ciclo-vida/ciclo-vida.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ciclo-vida',
   templateUrl: './ciclo-vida.component.html',
   styleUrls: ['./ciclo-vida.component.css']
 })
-export class CicloVidaComponent implements OnInit {
+export class CicloVidaComponent implements OnInit, OnChanges, DoCheck, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  @Input() valorInicial: number = 10;
 
-  ngOnInit(): void {
+  constructor() {
+    this.log('constructor')
   }
 
+  ngOnInit(): void {
+    this.log('ngOnInit')
+  }
+
+  ngOnChanges() {
+    this.log('ngOnChanges');
+  }
+
+  ngAfterContentInit() {
+    this.log('ngAfterContentInit')
+  }
+
+  ngDoCheck() {
+    this.log('ngDoCheck');
+  }
+
+  ngAfterContentChecked() {
+    this.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    this.log('ngAfterViewChecked')
+  }
+
+  ngOnDestroy() {
+    this.log('ngOnDestroy')
+  }
+
+
+  private log(hook: string) {
+    console.log(hook);
+  }
 }
